f6cebff75ab73a9d92814c3fb82e85897fb090b4
gabogarciam <gabogarciam71@gmail.com>
2020-09-30 21:28:53 +0200

    added routes again

--- server.js
+++ server.js
@@ -18,5 +18,15 @@ connection.once('open', () => {
     console.log('MongoDB DB connection established succefully');
 });
 
+// Routes
+app.get("/", (req, res) => {
+    res.send(`<h1>Exercise Tracker API</h1>Server is running on port: ${port}`);
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    res.status(404).json({code: 'not found'});
+});
+
 // start the server listening for requests
 app.listen(port, () => { console.log(`Server is running on port: ${port}`) });
