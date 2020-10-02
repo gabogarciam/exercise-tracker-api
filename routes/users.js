410c059124fef907c2b4c33d3380cc247e86ae81
gabogarciam <gabogarciam71@gmail.com>
2020-10-02 05:26:02 +0200

    fix catch newUser promise

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.route('/add').post((req, res) => {
 
     newUser.save()
         .then(() => res.json('User added seccessfully!!'))
-        .catch(() => res.status(400).json('Error: ' + err));
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
