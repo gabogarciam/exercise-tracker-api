932515d398a54417bfa7f7eb8d0d413b120f7827
gabogarciam <gabogarciam71@gmail.com>
2020-11-17 17:55:35 +0100

    Added eslint and prettier

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const uri = process.env.ATLAS_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((x) => console.log(`MongoDB DB connection established succefully, ${x.connections[0].name}`))
-    .catch(err => console.log('Error:', err));
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((x) =>
+    console.log(
+      `MongoDB DB connection established succefully, ${x.connections[0].name}`
+    )
+  )
+  .catch((err) => console.log("Error:", err));
--- routes/exercises.js
+++ routes/exercises.js
@@ -1,28 +1,29 @@
-const router = require('express').Router();
-const Exercise = require('../models/exercise.model');
+const router = require("express").Router();
+const Exercise = require("../models/exercise.model");
 
-router.route('/').get((req, res) => {
-    Exercise.find()
-        .then(exercices => res.json(exercices))
-        .catch(err => res.status(400).json('Erorr: ' + err));
+router.route("/").get((req, res) => {
+  Exercise.find()
+    .then((exercices) => res.json(exercices))
+    .catch((err) => res.status(400).json(`Erorr: ${err}`));
 });
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const description = req.body.description;
-    const duration = req.body.duration;
-    const date = Date.parse(req.body.date);
+router.route("/add").post((req, res) => {
+  const { username } = req.body;
+  const { description } = req.body;
+  const { duration } = req.body;
+  const date = Date.parse(req.body.date);
 
-    const newExercise = new Exercise({
-        username,
-        description,
-        duration,
-        date
-    });
+  const newExercise = new Exercise({
+    username,
+    description,
+    duration,
+    date,
+  });
 
-    newExercise.save()
-        .then(() => res.json('Exercise added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+  newExercise
+    .save()
+    .then(() => res.json("Exercise added!"))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 module.exports = router;
